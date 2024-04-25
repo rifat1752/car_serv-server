98c4e1011964d12ee98af856a64d069b8552114d
rifat1752 <rifathossain1752@gmail.com>
2024-04-26 01:52:44 +0600

    vercel deploy left

--- index.js
+++ index.js
@@ -16,6 +16,28 @@
   app.use(express.json());
   app.use(cookieParser());
 
+  // create middleware
+  const logger =async(req,res, next)=>{
+    console.log('called',req.host,req.originalUrl)
+    next()
+  }
+
+  const verifyToken = async(req,res,next)=>{
+    const token = req.cookies?.token;
+    console.log('token is ', token)
+    if(!token){
+      return res.status(401).send({message: "not authorized"})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({message: "not authorized"})
+      }
+      console.log('value decoded', decoded);
+      req.user = decoded;
+      next();
+    })
+    
+  }
 
 
 
@@ -39,27 +61,26 @@
       const bookingCollection = client.db('carserv').collection('bookings');
       
       //auth related api 
-      app.post('/jwt',async(req, res)=>{
+      app.post('/jwt',logger,async(req, res)=>{
         const user =req.body;
         console.log(user);
         const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
         res
         .cookie('token', token, {
           httpOnly:true,
-          secure:false,
-          
+          secure:false
         })
         .send({success: true});
       })
 
       //service related api
-      app.get('/services',async(req,res)=>{
+      app.get('/services',logger,async(req,res)=>{
           const cursor = serviceCollection.find();
           const result = await cursor.toArray();
           res.send(result);
       })
       app.get('/services/:id',async(req,res)=>{
-          const id = req.params.id;
+          const id = req.params.id; 
           const query = {_id: new ObjectId(id)};
           // const options = {
           //     projection : {_id:0, title:1,price:1, service_id:1}
@@ -70,9 +91,12 @@
       })
 
       //bookings
-      app.get('/bookings',async(req,res)=>{
+      app.get('/bookings',logger,verifyToken, async(req,res)=>{
         console.log(req.query.email);
-        console.log(" token", req.cookies.token)
+        console.log("user in the valid token", req.user)
+        if(req.query.email !== req.user.email){
+          return res.status(403).send({message:"forbidden access"})
+        }
         let query ={};
         if(req.query?.email){
           query = {email: req.query.email}
